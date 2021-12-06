7c65517a69fe04007b1c9636070bd353b139ae15
Tub-slug <94403100+Tub-slug@users.noreply.github.com>
2021-12-06 08:49:20 +0100

    more shit

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+def on_button_pressed_a():
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+    """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+    """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+    """)
+    basic.pause(100)
+    basic.show_leds("""
+        # . # . #
+                . # # # .
+                # # # # #
+                . # # # .
+                # . # . #
+    """)
+    basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    for index in range(5):
+        basic.show_icon(IconNames.SMALL_HEART)
+        basic.pause(100)
+        basic.show_icon(IconNames.HEART)
+        basic.pause(100)
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+    """)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_string("UPS")
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -31,4 +31,29 @@ basic.forever(function () {
         # . # . #
         `)
     basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("UPS")
 })
